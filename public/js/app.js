4bf08b06120e70a9e33d07dd8d4ba8b7e6240eaa
Tromesh Chalodiya <chalodiyatromesh@yahoo.com>
2019-11-29 16:59:20 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -20,16 +20,13 @@ weatherForm.addEventListener('submit', e => {
   const location = search.value;
   messageOne.textContent = 'Loading....';
   messageTwo.textContent = '';
-  fetch(
-    'http://localhost:3000/weather?address=' + encodeURIComponent(location),
-    {
-      method: 'GET',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'text/plain'
-      }
+  fetch('/weather?address=' + encodeURIComponent(location), {
+    method: 'GET',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'text/plain'
     }
-  ).then(res => {
+  }).then(res => {
     res.json().then(data => {
       if (data.error) {
         console.log(data.error);
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 const request = require('request');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Getting the geocode/weathercodefile
 const geocode = require('./utils/geocode');
@@ -110,6 +111,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('server is up on port 3000');
+app.listen(port, () => {
+  console.log('server is up on port ' + port);
 });
